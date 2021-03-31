3f3294a372cee688829bed7e6b4cca4c64ff6a54
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2021-03-31 10:04:58 -0400

    Add Laravel Breeze

---
+++ resources/views/theming.blade.php
@@ -0,0 +1,58 @@
+<x-guest-layout>
+<div class="sm:m-6 space-y-6">
+  <div class="relative max-w-4xl mx-auto overflow-hidden bg-skin-fill sm:rounded-2xl">
+    <img class="absolute inset-0 object-cover w-full h-full opacity-30" src="https://images.unsplash.com/photo-1613217784112-e0e197be6a0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80&sat=-100" alt="" />
+    <div class="absolute inset-0 bg-gradient-to-br from-skin-hue via-skin-hue to-transparent opacity-90"></div>
+    <div class="relative max-w-2xl px-4 py-16 mx-auto text-center sm:py-20 sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-extrabold text-skin-base sm:text-4xl">
+        <span class="block">Focus on your content.</span>
+        <span class="block">We handle the distribution.</span>
+      </h2>
+      <p class="mt-4 text-lg leading-6 text-skin-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo cupiditate excepturi facilis voluptatibus perspiciatis rem ullam repudiandae nobis. Et accusamus libero quo pariatur beatae temporibus ut fuga eum esse.</p>
+      <div class="max-w-sm mx-auto mt-10 sm:max-w-none sm:flex sm:justify-center">
+        <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+          <a href="" class="flex items-center justify-center px-4 py-3 text-base font-medium border border-transparent text-skin-inverted bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md shadow-sm sm:px-4">Get Started</a>
+          <a href="" class="flex items-center justify-center px-4 py-3 text-base font-medium border border-transparent text-skin-base bg-skin-button-muted rounded-md shadow-sm bg-opacity-60 hover:bg-opacity-70 sm:px-8">Live Demo</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="relative max-w-4xl mx-auto overflow-hidden theme-swiss bg-skin-fill sm:rounded-2xl">
+    <img class="absolute inset-0 object-cover w-full h-full opacity-30" src="https://images.unsplash.com/photo-1613217784112-e0e197be6a0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80&sat=-100" alt="" />
+    <div class="absolute inset-0 bg-gradient-to-br from-skin-hue via-skin-hue to-transparent opacity-90"></div>
+    <div class="relative max-w-2xl px-4 py-16 mx-auto text-center sm:py-20 sm:px-6 lg:px-8">
+      <h2 class="text-3xl font-extrabold text-skin-base sm:text-4xl">
+        <span class="block">Focus on your content.</span>
+        <span class="block">We handle the distribution.</span>
+      </h2>
+      <p class="mt-4 text-lg leading-6 text-skin-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo cupiditate excepturi facilis voluptatibus perspiciatis rem ullam repudiandae nobis. Et accusamus libero quo pariatur beatae temporibus ut fuga eum esse.</p>
+      <div class="max-w-sm mx-auto mt-10 sm:max-w-none sm:flex sm:justify-center">
+        <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+          <a href="" class="flex items-center justify-center px-4 py-3 text-base font-medium border border-transparent text-skin-inverted bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md shadow-sm sm:px-4">Get Started</a>
+          <a href="" class="flex items-center justify-center px-4 py-3 text-base font-medium border border-transparent text-skin-base bg-skin-button-muted rounded-md shadow-sm bg-opacity-60 hover:bg-opacity-70 sm:px-8">Live Demo</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="relative max-w-4xl mx-auto overflow-hidden theme-neon bg-skin-fill sm:rounded-2xl">
+    <img class="absolute inset-0 object-cover w-full h-full opacity-30" src="https://images.unsplash.com/photo-1613217784112-e0e197be6a0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80&sat=-100" alt="" />
+    <div class="absolute inset-0 bg-gradient-to-br from-skin-hue via-skin-hue to-transparent opacity-90"></div>
+    <div class="relative max-w-2xl px-4 py-16 mx-auto text-center sm:py-20 sm:px-6 lg:px-8">
+      <xl font-extrabold text-skin-base sm:text-4xl">
+        <span class="block">Focus on your content.</span>
+        <span class="block">We handle the distribution.</span>
+      </h2>
+      <p class="mt-4 text-lg leading-6 text-skin-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo cupiditate excepturi facilis voluptatibus perspiciatis rem ullam repudiandae nobis. Et accusamus libero quo pariatur beatae temporibus ut fuga eum esse.</p>
+      <div class="max-w-sm mx-auto mt-10 sm:max-w-none sm:flex sm:justify-center">
+        <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+          <a href="" class="flex items-center justify-center px-4 py-3 text-base font-medium border border-transparent text-skin-inverted bg-skin-button-accent hover:bg-skin-button-accent-hover rounded-md shadow-sm sm:px-4">Get Started</a>
+          <a href="" class="flex items-center justify-center px-4 py-3 text-base font-medium border border-transparent text-skin-base bg-skin-button-muted rounded-md shadow-sm bg-opacity-60 hover:bg-opacity-70 sm:px-8">Live Demo</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>h2 class="text-3
+</x-guest-layout>
+
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            textColor: {
+                skin: {
+                    base: withOpacity('--color-text-base'),
+                    muted: withOpacity('--color-text-muted'),
+                    inverted: withOpacity('--color-text-inverted'),
+                }
+            },
+            backgroundColor: {
+                skin: {
+                    fill: withOpacity('--color-fill'),
+                    'button-accent': withOpacity('--color-button-accent'),
+                    'button-accent-hover': withOpacity('--color-button-accent-hover'),
+                    'button-muted': withOpacity('--color-button-muted'),
+                }
+            },
+            gradientColorStops: {
+                skin: {
+                    hue: withOpacity('--color-fill'),
+                }
+            }
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
