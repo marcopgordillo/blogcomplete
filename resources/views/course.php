846c32f0535a710c9953326065cc86ac65417704
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2021-03-31 19:55:04 -0400

    Add data types details

--- resources/views/course.php
+++ resources/views/course.php
@@ -1,48 +1,145 @@
 <?php
 declare(strict_types=1);
-# Scalar types
 
-# bool - true / false
-$completed = true;
+/* BOOLEANS */
 
-# int -1, 2, 3, 0, -5 (no decimal)
-$score = 75;
+$isComplete = false;
 
-#float 1.5, 0.1, 0.005, -15.5
-$price = 0.99;
+/* FALSYES
+ *
+ * integers 0 -0 = false
+ * floats 0.0 -0.0 = false
+ * '' = false
+ * [] = false
+ * null = false
+ *
+ * */
 
-#string - "Marco"
-$greeting = "Hello Marco";
-
-echo $completed . '<br>';
-echo $score . '<br>';
-echo $price . '<br>';
-echo $greeting . '<br>';
-
-# Compound Types
-# Array
-$companies = [1, 2, 3, 0.5, -9.2, 'A', 'b', true];
-print_r($companies);
-
-# Object
-# Callable
-# Iterable
-
-# Special types
-# resource
-# null
-
-function sum(float $x, float $y)
-{
-    return $x + $y;
+if ($isComplete) {
+    echo 'success';
+} else {
+    echo 'fail';
 }
 
-// $sum = sum(2, '3');
-$sum = sum(2.5, 3.4);
+/* INTEGERS */
 
-echo $sum . '<br>';
+$x = 50; // Decimal
+$y = 0x3a; // Hexadecimal
+$z = 024; // Octal
+$b = 0b1101; // Binary
 
-var_dump($sum);
+echo $x, $y, $z, $b;
 
-$x = (int) '5';
+echo '<br>' . PHP_INT_MAX;
+echo '<br>' . PHP_INT_MIN;
+
+$x = 200_000; // readibility
+echo '<br>' . $x;
+
+/* FLOATS */
+
+$x = 13_000.5;
+echo '<br>' . $x;
+echo '<br>' . PHP_FLOAT_MAX;
+echo '<br>' . PHP_FLOAT_MIN;
+
+$x = floor((0.1 + 0.7) * 10); // 7.9999999999991118 The presicion
+echo '<br>' . $x;
+$x = ceil((0.1 + 0.7) * 10); // 7.9999999999991118 The presicion
+echo '<br>' . $x;
+$x = ceil((0.1 + 0.2) * 10); // 3.000000000000000004 The presicion
+echo '<br>' . $x . '<br>';
+
+$x = 0.23;
+$y = 1 - 0.77;
+
+var_dump($x, $y);
+
+if ($x == $y) {
+    echo 'yes';
+} else {
+    echo 'no';
+}
+
+echo NAN;
+echo log(-1);
+$x = log(-1);
 var_dump($x);
+var_dump(is_nan($x));
+echo INF;
+echo PHP_FLOAT_MAX * 2;
+
+$x = PHP_FLOAT_MAX * 2;
+var_dump($x);
+var_dump(is_infinite($x));
+var_dump(is_finite($x));
+
+echo null;
+
+/* CASTINGS */
+
+echo '<br>';
+$x = 5;
+$x = 'dsdsdd';
+$x = '4.5sds';
+
+
+var_dump((float) $x);
+
+/* STRINGS */
+
+$firstName = 'Will';
+$lastName = 'Smith';
+
+$name = $firstName . ' ' . $lastName;
+$name = "Mr. $firstName {$lastName}";
+
+echo '<br>';
+echo $name;
+echo $name[0];
+echo "<br>Letther 2: $name[1]<br>";
+
+$x = 1;
+$y = 2;
+
+// Heredoc
+$text = <<<TEXT
+Line 1 $x
+Line 2 $y
+Line 3 ' "
+<br>
+TEXT;
+
+echo $text . '<br>';
+echo nl2br($text);
+
+// Nowdoc
+$text = <<<'TEXT'
+Line 1
+Line 2
+Line 3 ' "
+<br>
+TEXT;
+
+echo nl2br($text);
+
+$text = <<<TEXT
+<div>
+    <p>Hello world!</p>
+</div>
+<br>
+TEXT;
+
+echo nl2br($text);
+
+// NULL
+$x = null;
+
+echo $x;
+var_dump(is_null($x));
+
+$x = 123;
+var_dump($x);
+unset($x);
+// var_dump($x);
+
