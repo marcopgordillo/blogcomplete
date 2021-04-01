67dd745b76e98e9ca4768882eeb7ccf959e07476
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2021-03-31 23:07:41 -0400

    Operators

--- resources/views/course.php
+++ resources/views/course.php
@@ -9,62 +9,18 @@ function print_array(array $array_to_print) {
     echo count($array_to_print);
 }
 
-/* ARRAYS */
-$programmingLanguages = ['PHP', 'Java', 'Python'];
+/* OPERATORS */
 
-print_array($programmingLanguages);
+// Aritmetic Operators
+$x = +'10';
+$x = 10;
+$y = 2;
 
-array_push($programmingLanguages, 'C', 'JavaScript');
-$programmingLanguages[] = 'C++';
+var_dump($x - $y);
+var_dump($x / $y);
+var_dump(fdiv($x, $y));
+var_dump($x % $y);
+var_dump(fmod($x, $y));
 
-print_array($programmingLanguages);
-
-$programmingLanguages = [
-    'php' => '8.0',
-    'python' => '3.9',
-];
-
-print_array($programmingLanguages);
-
-$newLanguage = 'go';
-
-$programmingLanguages[$newLanguage] = '1.15';
-
-print_array($programmingLanguages);
-
-$array = [true => 'a', 1 => 'b', '1' => 'c', 1.8 => 'd', null => 'e'];
-
-print_array($array);
-
-echo $array[null];
-
-echo array_pop($array);
-print_array($array);
-
-$array = ['a', 'b', 50 => 'c', 'd', 'e'];
-
-print_array($array);
-
-echo array_shift($array);
-print_array($array);
-
-// unset($array);
-unset($array[50]);
-print_array($array);
-
+// Assignments Operators
 $x = 5;
-
-$array = (array) $x; //cast
-
-print_array($array);
-
-$array = ['a' => 1, 'b' => null];
-
-var_dump(array_key_exists('a', $array));
-var_dump(isset($array['a']));
-
-var_dump(array_key_exists('b', $array));
-var_dump(isset($array['b']));
-
-print_array($array);
-
