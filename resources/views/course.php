d333f1ba5feafcc9fcb43e89a8206550e671cba4
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2021-04-01 00:10:08 -0400

    Operators

--- resources/views/course.php
+++ resources/views/course.php
@@ -17,6 +17,8 @@ $x = 10;
 $y = 2;
 
 var_dump($x - $y);
+var_dump($x * $y);
+var_dump($x ** $y);
 var_dump($x / $y);
 var_dump(fdiv($x, $y));
 var_dump($x % $y);
@@ -24,3 +26,33 @@ var_dump(fmod($x, $y));
 
 // Assignments Operators
 $x = 5;
+
+// Comparison Operators
+$x = 11;
+$y = 10;
+
+var_dump($x <=> $y);
+
+var_dump(0 == 'hello');
+var_dump(0 == '0');
+
+$x = 'Hello World';
+$y = strpos($x, 'H');
+
+// if ($y === false) {
+    // echo 'H Not Found';
+// } else {
+    // echo 'H Found at index ' . $y;
+// }
+echo '<br>';
+var_dump($y);
+
+$result = $y === false ? 'H Not Found' : 'H Found at index ' . $y;
+echo $result;
+
+echo '<br>';
+
+// $z = null;
+$y = $z ?? 'hello';
+
+var_dump($y);
